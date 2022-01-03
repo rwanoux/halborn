eeaadd53f1c61b59eabcce0fc3b9ec04bdb9b2d5
rwanoux <erwan_lemaire@yahoo.fr>
2022-01-03 10:47:02 +0100

    pouf

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
-var options = {
-    strings: [' ', 'ELITE <br> BLOCKCHAIN <br> CYBERSECURITY'],
-    typeSpeed: 20
-};
-
-var typed = new Typed('#page-title', options);
-
-
-
 let sections = document.getElementsByTagName("section");
-window.addEventListener('scroll', appearSection);
-window.addEventListener('scroll', scaleHeader);
+let title = document.getElementById("page-title");
+window.onload = function() {
+    type()
+}
+window.addEventListener('scroll', scrollManagement)
+
+
+function scrollManagement() {
+    appearSection();
+    scaleHeader();
+    cardAppear();
+    if (window.scrollY > 0 && window.scrollY < 15) {
+        type()
+    }
+}
+
+function cardAppear() {
+    let cards = document.getElementsByClassName("card");
+    for (let crd of cards) {
+        if (crd.getBoundingClientRect().bottom < window.innerHeight) {
+            crd.classList.add("visible")
+        } else {
+            crd.classList.remove("visible")
+        }
+    }
+
+}
 
 function appearSection() {
 
     for (let sc of sections) {
         let rect = sc.getBoundingClientRect()
-        console.log(rect.bottom - rect.height, sc, sc.id);
         if (rect.bottom - rect.height < window.innerHeight / 1.7) {
             sc.classList.add("visible")
         } else {
@@ -23,10 +38,28 @@ function appearSection() {
 
         }
     }
+
+
+
+}
+
+function clearType() {
+    if (title.nextElementSibling) {
+        title.parentNode.removeChild(title.nextElementSibling);
+        title.innerHTML = ""
+    }
+}
+
+function type() {
+    clearType()
+    var options = {
+        strings: [' ', 'ELITE <br> BLOCKCHAIN <br> CYBERSECURITY'],
+        typeSpeed: 20
+    };
+    var typed = new Typed('#page-title', options);
 }
 
 function scaleHeader() {
-    console.log(window.scrollY)
     if (window.scrollY > 100) {
         document.getElementById('scalableHeader').classList.add('mini')
     } else {
